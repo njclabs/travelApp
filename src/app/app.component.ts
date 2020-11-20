b96c0396e81c3884fe9a079f9d74ed3529b1ea49
vikas <vikas_puri1886@outlook.com>
2020-11-20 16:10:25 +0000

    first commit of travel app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'travelApp';
+
+  public fetchTravelData(origin: any,destination: any,travelDate: any){
+    console.log(origin.value);
+  }
 }
