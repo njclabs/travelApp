c9465899ed8852e7443c787026f1c493d27f9e64
vikas <vikas_puri1886@outlook.com>
2020-11-22 19:01:34 +0000

    draft layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'travelApp';
+  appService=null;
+  public locations = []
+  public trainData = []
+  public covidData = []
+  public showWeatherAndCovidData = false;
+
+
+  constructor(appService: AppService){
+    this.appService = appService;  
+  }
+
+  ngOnInit() {
+    this.locations = this.appService.getLocations();
+    this.covidData = this.appService.fetchCovidData();
+  }
 
   public fetchTravelData(origin: any,destination: any,travelDate: any){
-    console.log(origin.value);
+    this.trainData = this.appService.fetchTrainData(origin,destination,travelDate)
+    this.showWeatherAndCovidData = true;
   }
 }
